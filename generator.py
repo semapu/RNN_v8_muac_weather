4a2efb9f0365844be81db309fcbeb05f42be2236
sergi <sergimasp@gmail.com>
2021-06-13 16:20:19 +0200

    Generator used for C-ACT Capacity regulations

--- generator.py
+++ generator.py
@@ -10,6 +10,7 @@ import sys
 import numpy as np
 import random
 from typing import List
+from datetime import datetime
 
 from utils_samplesTraining import readAssociatedFile_fromAIRAC_givenDate, \
                                   extract_regulations, \
@@ -27,7 +28,7 @@ def extract_days_timestamps_volumes_labels_days_with_regulations(list_dates_with
                                                                  num_additional_samples_per_day: int,
                                                                  min_timestamps_with_regulations: int):
     """
-    Function that given the information from the regulations recorded, it computes samples from those days
+    Function that given the information from the regulations recorded it computes samples from those days
     WITH and WITHOUT regulations.
 
     Args:
@@ -98,7 +99,7 @@ def extract_days_timestamps_volumes_labels_days_with_regulations(list_dates_with
                                                timestamps_end_for_that_day_with_regulations,
                                                volumes_for_that_day_with_regulations):
 
-                # Check if the sectors volumes are coincident
+                # Check if the sectors volumes are coincidentg
                 if vol == volume:
                     start_minutes_regulation = from_HHMMSS_to_HH(start_reg) * 60 + int(str(start_reg)[-4:-2])
                     end_minutes_regulation = from_HHMMSS_to_HH(end_reg) * 60 + int(str(end_reg)[-4:-2])
@@ -182,7 +183,7 @@ def extract_features_from_list_days_and_timestamps(list_days,
     if isinstance(traffic_volume, str):
         traffic_volume = [traffic_volume] * list_days.shape[0]
 
-    # Sanity check: Ensuring the list "sector_name" has the correct lenlist_days_from_regulationsgth
+    # Sanity check: Ensuring the list "sector_name" has the correct length
     if len(traffic_volume) != list_days.shape[0]:
         sys.exit("The length of both days opf study and volumes must be the same. "
                  "[extract_features_from_list_days_and_timestamps]")
@@ -193,13 +194,13 @@ def extract_features_from_list_days_and_timestamps(list_days,
         # Read the correct files according to the date of interest
         # AIRACs only contain information from an interval of 28 days
 
-        # normal_capacity, regulations, oc, ec_20_20, ec_60_20, \
-        # workload_1_1, workload_20_20, num_conflicts_1_1, \
-        # climbing_1_1, cruising_1_1, descending_1_1 = readAssociatedFile_fromAIRAC_givenDate(day)
-
         normal_capacity, regulations, oc, ec_20_20, ec_60_20, \
-        workload_1_1, workload_20_20, \
-        climbing_1_1, cruising_1_1, descending_1_1 = readAssociatedFile_fromAIRAC_givenDate(day)
+         workload_1_1, workload_20_20, num_conflicts_1_1, \
+         climbing_1_1, cruising_1_1, descending_1_1 = readAssociatedFile_fromAIRAC_givenDate(day)
+
+        # normal_capacity, regulations, oc, ec_20_20, ec_60_20, \
+        #  workload_1_1, workload_20_20, \
+        #  climbing_1_1, cruising_1_1, descending_1_1 = readAssociatedFile_fromAIRAC_givenDate(day)
 
         starting_index_inside_day = int(start[0:2]) * 60 + int(start[2:4])
         end_index_inside_day = int(end[0:2]) * 60 + int(end[2:4])
@@ -208,22 +209,26 @@ def extract_features_from_list_days_and_timestamps(list_days,
         interval_day_associated_timestamp = math.floor(starting_index_inside_day / (gap_before_start_time + gap_after_start_time))
         X[counter_day][:][:, 0] = interval_day_associated_timestamp
 
+        """ Day of the week """
+        day_week = datetime.strptime(day, "%d/%m/%Y").weekday()
+        X[counter_day][:][:, 1] = day_week
+
         """ Capacity """
         # Extract the regulation for the given date
         # intervals_regulation, capacity_dueTo_regulation = extract_regulations(sector_name, day, regulations)
 
-        # Temporal capacity
+        # Using the nominal capacity
+        delta_timesteps = 1  # Difference en minutes between the timestamps in the sample
+        tmp_capacity = np.zeros((int(24 * 60 / delta_timesteps)))
+
         # Add the regulations to the temporal capacity vector
         # tmp_capacity = addIntervals_toFinalConjunt(intervals_regulation, capacity_dueTo_regulation, 1)
-
-        delta_timesteps = 1  # Difference en minutes between the timestamps in the sample
-        tmp_capacity = np.zeros((int(24*60/delta_timesteps)))
 
         # Add the usual capacity to the temporal capacity
         tmp_capacity[tmp_capacity == 0] = normal_capacity[(normal_capacity.iloc[:, 0] == day) &
                                                           (normal_capacity.iloc[:, 1] == 'MAS' + sector_name)][4].values[0]
 
-        X[counter_day][:][:, 1] = tmp_capacity[starting_index_inside_day:end_index_inside_day]
+        X[counter_day][:][:, 2] = tmp_capacity[starting_index_inside_day:end_index_inside_day]
 
         """ Occupancy count """
         # Filtering the timestamps and the sector
@@ -233,16 +238,16 @@ def extract_features_from_list_days_and_timestamps(list_days,
         if len(oc_atDate) == 0:
             sys.exit("No OC were found for the date: " + str(day))
 
-        X[counter_day][:][:, 2] = oc_atDate[starting_index_inside_day:end_index_inside_day]
+        X[counter_day][:][:, 3] = oc_atDate[starting_index_inside_day:end_index_inside_day]
 
         """ Entry count 20/20 """
         # Filtering the datestamp and the sector
-        ec_20_20_atDate = ec_20_20[(ec_20_20.iloc[:, 0] == day) & (ec_20_20.iloc[:, 2] == 'EDYY' + sector_name)].values[0][5:-1]
+        ec_20_20_atDate = ec_20_20[(ec_20_20.iloc[:, 0] == day) & (ec_20_20.iloc[:, 2] == 'EDYY' + sector_name)].values[0][6:-1]
 
         for i in range(0, gap_before_start_time + gap_after_start_time):
             # ec_20_20 has a frequency of 20 minutes
             index_value = math.floor((starting_index_inside_day + i) / 20)
-            X[counter_day][:][i, 3] = ec_20_20_atDate[index_value]
+            X[counter_day][:][i, 4] = ec_20_20_atDate[index_value]
 
         # Sanity check - Ensure only one row is obtained after the filtering
         if len(ec_20_20_atDate) == 0:
@@ -250,12 +255,12 @@ def extract_features_from_list_days_and_timestamps(list_days,
 
         """ Entry count 60/20 """
         # Filtering the datastamp and the sector
-        ec_60_20_atDate = ec_60_20[(ec_60_20.iloc[:, 0] == day) & (ec_60_20.iloc[:, 2] == 'EDYY' + sector_name)].values[0][5:-1]
+        ec_60_20_atDate = ec_60_20[(ec_60_20.iloc[:, 0] == day) & (ec_60_20.iloc[:, 2] == 'EDYY' + sector_name)].values[0][6:-1]
 
         for i in range(0, gap_before_start_time + gap_after_start_time):
             # ec_60_20 has a frequency of 20 minutes
             index_value = math.floor((starting_index_inside_day + i) / 20)
-            X[counter_day][:][i, 4] = ec_60_20_atDate[index_value]
+            X[counter_day][:][i, 5] = ec_60_20_atDate[index_value]
 
         # Sanity check - Ensure only one row is obtained after the filtering
         if len(ec_60_20_atDate) == 0:
@@ -263,66 +268,66 @@ def extract_features_from_list_days_and_timestamps(list_days,
 
         """ Workload_1_1 """
         # Filtering the timestamps and the sector
-        workload_at_date = workload_1_1[(workload_1_1.iloc[:, 0] == day) & (workload_1_1.iloc[:, 2] == 'EDYY' + sector_name)].values[0][5:-1]
+        workload_at_date = workload_1_1[(workload_1_1.iloc[:, 0] == day) & (workload_1_1.iloc[:, 2] == 'EDYY' + sector_name)].values[0][6:-1]
 
         # Sanity check - Ensure only one row is obtained after the filtering
         if len(workload_at_date) == 0:
             sys.exit("No workload (1 1) were found for the date: " + str(day))
 
-        X[counter_day][:][:, 5] = workload_at_date[starting_index_inside_day:end_index_inside_day]
+        X[counter_day][:][:, 6] = workload_at_date[starting_index_inside_day:end_index_inside_day]
 
         """ Workload_20_20 """
         # Filtering the timestamp and the sector
-        workload_20_20_atDate = workload_20_20[(workload_20_20.iloc[:, 0] == day) & (workload_20_20.iloc[:, 2] == 'EDYY' + sector_name)].values[0][5:-1]
+        workload_20_20_atDate = workload_20_20[(workload_20_20.iloc[:, 0] == day) & (workload_20_20.iloc[:, 2] == 'EDYY' + sector_name)].values[0][6:-1]
 
         for i in range(0, gap_before_start_time + gap_after_start_time):
             # ec_20_20 has a frequency of 20 minutes
             index_value = math.floor((starting_index_inside_day + i) / 20)
-            X[counter_day][:][i, 6] = workload_20_20_atDate[index_value]
+            X[counter_day][:][i, 7] = workload_20_20_atDate[index_value]
 
         # Sanity check - Ensure only one row is obtained after the filtering
         if len(workload_20_20_atDate) == 0:
             sys.exit("No workload (20 20) was found for the date: " + str(day))
 
-        # """ Number of conflicts """
-        # # Filtering the timestamps and the sector
-        # num_conflicts_at_date = num_conflicts_1_1[(num_conflicts_1_1.iloc[:, 0] == day) & (num_conflicts_1_1.iloc[:, 2] == 'EDYY' + sector_name)].values[0][5:-1]
-        #
-        # # Sanity check - Ensure only one row is obtained after the filtering
-        # if len(num_conflicts_at_date) == 0:
-        #     sys.exit("No Num. conflicts were found for the date: " + str(day))
-        #
-        # X[counter_day][:][:, 7] = num_conflicts_at_date[starting_index_inside_day:end_index_inside_day]
+        """ Number of conflicts """
+        # Filtering the timestamps and the sector
+        num_conflicts_at_date = num_conflicts_1_1[(num_conflicts_1_1.iloc[:, 0] == day) & (num_conflicts_1_1.iloc[:, 2] == 'EDYY' + sector_name)].values[0][5:-1]
+
+        # Sanity check - Ensure only one row is obtained after the filtering
+        if len(num_conflicts_at_date) == 0:
+            sys.exit("No Num. conflicts were found for the date: " + str(day))
+
+        X[counter_day][:][:, 8] = num_conflicts_at_date[starting_index_inside_day:end_index_inside_day]
 
         """ Climbing_1_1 """
         # Filtering the timestamps and the sector
-        climbing_at_date = climbing_1_1[(climbing_1_1.iloc[:, 0] == day) & (climbing_1_1.iloc[:, 2] == 'EDYY' + sector_name)].values[0][5:-1]
+        climbing_at_date = climbing_1_1[(climbing_1_1.iloc[:, 0] == day) & (climbing_1_1.iloc[:, 2] == 'EDYY' + sector_name)].values[0][6:-1]
 
         # Sanity check - Ensure only one row is obtained after the filtering
         if len(climbing_at_date) == 0:
             sys.exit("No climbing were found for the date: " + str(day))
 
-        X[counter_day][:][:, 8] = climbing_at_date[starting_index_inside_day:end_index_inside_day]
+        X[counter_day][:][:, 9] = climbing_at_date[starting_index_inside_day:end_index_inside_day]
 
         """ Cruising_1_1 """
         # Filtering the timestamps and the sector
-        cruising_at_date = cruising_1_1[(cruising_1_1.iloc[:, 0] == day) & (cruising_1_1.iloc[:, 2] == 'EDYY' + sector_name)].values[0][5:-1]
+        cruising_at_date = cruising_1_1[(cruising_1_1.iloc[:, 0] == day) & (cruising_1_1.iloc[:, 2] == 'EDYY' + sector_name)].values[0][6:-1]
 
         # Sanity check - Ensure only one row is obtained after the filtering
         if len(climbing_at_date) == 0:
             sys.exit("No cruising time were found for the date: " + str(day))
 
-        X[counter_day][:][:, 9] = cruising_at_date[starting_index_inside_day:end_index_inside_day]
+        X[counter_day][:][:, 10] = cruising_at_date[starting_index_inside_day:end_index_inside_day]
 
         """ Descending_1_1 """
         # Filtering the timestamps and the sector
-        descending_at_date = descending_1_1[(descending_1_1.iloc[:, 0] == day) & (descending_1_1.iloc[:, 2] == 'EDYY' + sector_name)].values[0][5:-1]
+        descending_at_date = descending_1_1[(descending_1_1.iloc[:, 0] == day) & (descending_1_1.iloc[:, 2] == 'EDYY' + sector_name)].values[0][6:-1]
 
         # Sanity check - Ensure only one row is obtained after the filtering
         if len(workload_at_date) == 0:
             sys.exit("No descending time were found for the date: " + str(day))
 
-        X[counter_day][:][:, 10] = descending_at_date[starting_index_inside_day:end_index_inside_day]
+        X[counter_day][:][:, 11] = descending_at_date[starting_index_inside_day:end_index_inside_day]
 
         ##################################
         # Increase the counting of the day
